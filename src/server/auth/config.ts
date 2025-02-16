0c915e43397af1f56a4c946250f64239714c58df
sahandsn <sanaeisahand@gmail.com>
2025-02-17 00:22:35 +0330

    feat: run everything

--- src/server/auth/config.ts
+++ src/server/auth/config.ts
@@ -64,4 +64,5 @@ export const authConfig = {
       },
     }),
   },
+  trustHost: true,
 } satisfies NextAuthConfig;
--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -1,18 +1,17 @@
-import { drizzle } from "drizzle-orm/postgres-js";
-import postgres from "postgres";
+import { drizzle } from "drizzle-orm/neon-http";
+import { neon } from "@neondatabase/serverless";
 
 import { env } from "@/env";
 import * as schema from "./schema";
 
 /**
- * Cache the database connection in development. This avoids creating a new connection on every HMR
- * update.
+ * Cache the Neon connection in development. This avoids creating new connections on HMR updates.
  */
 const globalForDb = globalThis as unknown as {
-  conn: postgres.Sql | undefined;
+  conn: ReturnType<typeof neon> | undefined;
 };
 
-const conn = globalForDb.conn ?? postgres(env.DATABASE_URL);
+const conn = globalForDb.conn ?? neon(env.DATABASE_URL);
 if (env.NODE_ENV !== "production") globalForDb.conn = conn;
 
 export const db = drizzle(conn, { schema });
