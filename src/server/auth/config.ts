50821bb2af98b784d57e37f974d094bc1aa0beb6
sahandsn <sanaeisahand@gmail.com>
2025-02-18 23:32:37 +0330

    fix: drizzle orm

--- src/server/auth/config.ts
+++ src/server/auth/config.ts
@@ -20,15 +20,10 @@ declare module "next-auth" {
   interface Session extends DefaultSession {
     user: {
       id: string;
-      // ...other properties
-      // role: UserRole;
+      // business logic
+      proVerified: boolean;
     } & DefaultSession["user"];
   }
-
-  // interface User {
-  //   // ...other properties
-  //   // role: UserRole;
-  // }
 }
 
 /**
--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -7,8 +7,10 @@ import {
   text,
   timestamp,
   varchar,
+  boolean,
 } from "drizzle-orm/pg-core";
 import { type AdapterAccount } from "next-auth/adapters";
+import { nanoid } from "nanoid";
 
 /**
  * This is an example of how to use the multi-project schema feature of Drizzle ORM. Use the same
@@ -30,13 +32,13 @@ export const posts = createTable(
       .default(sql`CURRENT_TIMESTAMP`)
       .notNull(),
     updatedAt: timestamp("updated_at", { withTimezone: true }).$onUpdate(
-      () => new Date()
+      () => new Date(),
     ),
   },
   (example) => ({
     createdByIdIdx: index("created_by_idx").on(example.createdById),
     nameIndex: index("name_idx").on(example.name),
-  })
+  }),
 );
 
 export const users = createTable("user", {
@@ -51,10 +53,14 @@ export const users = createTable("user", {
     withTimezone: true,
   }).default(sql`CURRENT_TIMESTAMP`),
   image: varchar("image", { length: 255 }),
+  // business logic
+  proVerified: boolean("pro_verified").default(false).notNull(),
 });
 
 export const usersRelations = relations(users, ({ many }) => ({
   accounts: many(accounts),
+  urls: many(users),
+  referrals: many(referrals),
 }));
 
 export const accounts = createTable(
@@ -83,7 +89,7 @@ export const accounts = createTable(
       columns: [account.provider, account.providerAccountId],
     }),
     userIdIdx: index("account_user_id_idx").on(account.userId),
-  })
+  }),
 );
 
 export const accountsRelations = relations(accounts, ({ one }) => ({
@@ -106,7 +112,7 @@ export const sessions = createTable(
   },
   (session) => ({
     userIdIdx: index("session_user_id_idx").on(session.userId),
-  })
+  }),
 );
 
 export const sessionsRelations = relations(sessions, ({ one }) => ({
@@ -125,5 +131,113 @@ export const verificationTokens = createTable(
   },
   (vt) => ({
     compoundKey: primaryKey({ columns: [vt.identifier, vt.token] }),
-  })
+  }),
 );
+
+// business tables
+
+export const urls = createTable(
+  "url",
+  {
+    id: varchar("id", { length: 255 })
+      .notNull()
+      .primaryKey()
+      .$defaultFn(() => crypto.randomUUID()),
+    userId: varchar("user_id", { length: 255 })
+      .references(() => users.id)
+      .notNull(),
+    createdAt: timestamp("created_at", { withTimezone: true })
+      .default(sql`CURRENT_TIMESTAMP`)
+      .notNull(),
+    updatedAt: timestamp("updated_at", { withTimezone: true }).$onUpdate(
+      () => new Date(),
+    ),
+    expiresAt: timestamp("expires_at", { withTimezone: true }),
+    genericDestination: varchar("generic_destination", { length: 6 })
+      .unique()
+      .notNull()
+      .$defaultFn(() => nanoid(6)),
+    customDestination: varchar("custom_destination", { length: 255 }).unique(),
+    source: varchar("source").notNull(),
+  },
+  (url) => ({
+    genericDestinationIdx: index("generic_destination_idx").on(
+      url.genericDestination,
+    ),
+    customDestinationIdx: index("custom_destination_idx").on(
+      url.customDestination,
+    ),
+    userIdIdx: index("user_id_idx").on(url.userId),
+  }),
+);
+
+export const urlRelations = relations(urls, ({ one, many }) => ({
+  user: one(users, {
+    fields: [urls.userId],
+    references: [users.id],
+  }),
+  urlAnalytics: many(urlAnalytics),
+}));
+
+export const urlAnalytics = createTable(
+  "url_analytic",
+  {
+    id: varchar("id", { length: 255 })
+      .primaryKey()
+      .$defaultFn(() => crypto.randomUUID()),
+    urlId: varchar("url_id", { length: 255 })
+      .references(() => urls.id)
+      .notNull(),
+    timestamp: timestamp("timestamp", { withTimezone: true })
+      .default(sql`CURRENT_TIMESTAMP`)
+      .notNull(),
+  },
+  (analytic) => ({
+    urlIdIdx: index("url_analytic_id_idx").on(analytic.urlId),
+    timestampIdx: index("url_analytic_timestamp_idx").on(analytic.timestamp),
+  }),
+);
+
+export const urlAnalyticsRelations = relations(urlAnalytics, ({ one }) => ({
+  url: one(urls, {
+    fields: [urlAnalytics.urlId],
+    references: [urls.id],
+  }),
+}));
+
+export const referrals = createTable(
+  "referral",
+  {
+    id: varchar("id", { length: 255 })
+      .notNull()
+      .primaryKey()
+      .$defaultFn(() => crypto.randomUUID()),
+    referrerId: varchar("referrer_id", { length: 255 })
+      .notNull()
+      .references(() => users.id),
+    referredId: varchar("referred_id", { length: 255 }).references(
+      () => users.id,
+    ),
+    createdAt: timestamp("created_at", { withTimezone: true })
+      .default(sql`CURRENT_TIMESTAMP`)
+      .notNull(),
+    updatedAt: timestamp("updated_at", { withTimezone: true }).$onUpdate(
+      () => new Date(),
+    ),
+  },
+  (referral) => ({
+    referrerIdIdx: index("referrer_id_idx").on(referral.referrerId),
+    referredIdIdx: index("referred_id_idx").on(referral.referredId),
+  }),
+);
+
+export const referralsRelations = relations(referrals, ({ one }) => ({
+  referrer: one(users, {
+    fields: [referrals.referrerId],
+    references: [users.id],
+  }),
+  referred: one(users, {
+    fields: [referrals.referredId],
+    references: [users.id],
+  }),
+}));
