2f8a9f3819b61502e4681ea22e960a774009d3a6
sahandsn <sanaeisahand@gmail.com>
2025-02-21 12:37:11 +0330

    fix: unused code

--- src/app/[id]/page.tsx
+++ src/app/[id]/page.tsx
@@ -11,7 +11,6 @@ export default async function UrlPage({
 
   try {
     url = await api.url.createAnalytics({ destination: id });
-    console.log(url.source);
   } catch (error) {
     console.error(error);
     redirect("/not-found");
